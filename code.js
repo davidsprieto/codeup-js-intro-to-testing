d7bbddd16087209902bd48252d914fa1b4cd0126
David Prieto <david@Davids-MBP.hsd1.il.comcast.net>
2022-03-23 11:38:28 -0500

    completed exercise #3 for intro-to-testing.

--- code.js
+++ code.js
@@ -5,6 +5,6 @@ function helloWorld() {
 
 // sayHello function
 function sayHello() {
-
+    return "Hello, Jane!";
 }
 
--- tests.js
+++ tests.js
@@ -16,7 +16,13 @@ describe('helloWorld', function() {
 
 describe('sayHello', function() {
     it('should be a defined function', function() {
-        expect(typeof sayHello()).toBe('function');
+        expect(typeof sayHello).toBe('function');
     });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello()).toBe("Hello, Jane!");
+    })
 
 })
